4ee593410a7ef381d1643d46c2700df8536ac16a
Mohammadjavad <mohamadjavad.jalili05@gmail.com>
2024-12-11 21:25:46 +0330

    edit

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-!w(#0$ymui_@xfs=dpqfju*q^qr*)$f-)ig8jf9!0h1nt@ueiy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
